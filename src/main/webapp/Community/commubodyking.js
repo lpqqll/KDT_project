886bef24d4b786f2046c5e63e5ec5f4eb0a46f0f
채이스내 <dorothy922@naver.com>
2023-09-11 15:27:50 +0900

    ㅁㄴㅇㄹ

--- src/main/webapp/Community/commubodyking.js
+++ src/main/webapp/Community/commubodyking.js
@@ -3,32 +3,32 @@ let commubodyking = document.getElementById("list4");
 let listArr4 = new Array(10);
 listArr4 = [
     {
-        title : "9/6 오.운.완",
+        title : "제 등근육 보실?",
         vote : 12,
         writer : "민수아빠",
         writedDate : "2023-09-06"
     },{
-        title : "오늘도 인증합니다.",
+        title : "헬스 3년차 몸 변화",
         vote : 7,
         writer : "김현주",
         writedDate : "2023-09-06"
     },{
-        title : "결국 와버렸다...",
+        title : "복근득근",
         vote : 25,
         writer : "근육킹",
         writedDate : "2023-09-06"
     },{
-        title : "오운완 인증",
+        title : "멸치 인증",
         vote : 16,
         writer : "나는 슈퍼맨",
         writedDate : "2023-09-06"
     },{
-        title : "41번째 오.운.완",
+        title : "삼두 인증",
         vote : 31,
         writer : "김현주",
         writedDate : "2023-09-06"
     },{
-        title : "오늘은 하체 하는 날~",
+        title : "바프 찍었습니다.",
         vote : 25,
         writer : "김계란",
         writedDate : "2023-09-06"
