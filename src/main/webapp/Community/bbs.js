2365276ac3e9fa8a8f7332edd864b3bb515e2e54
채이스내 <dorothy922@naver.com>
2023-09-11 09:48:18 +0900

    ㅁㄴㅇㄹ

--- src/main/webapp/Community/bbs.js
+++ src/main/webapp/Community/bbs.js
@@ -8,14 +8,14 @@ listArr = [
         writer : "나는 슈퍼맨",
         writedDate : "2023-09-06"
     },{
-        title : "헬창놈들 파이팅",
+        title : "왕이 넘어지면?",
         vote : 15,
-        writer : "채이스내",
+        writer : "아재개그러버",
         writedDate : "2023-09-06"
     },{
-        title : "프로틴 유머",
-        vote : 20,
-        writer : "이건아",
+        title : "ㅇwㅇ",
+        vote : 12,
+        writer : "근육맨",
         writedDate : "2023-09-06"
     },{
         title : "ㅋㅋㅋㅋ",
@@ -38,19 +38,19 @@ listArr = [
         writer : "나는 슈퍼맨",
         writedDate : "2023-09-06"
     },{
-        title : "헬창놈들 파이팅",
+        title : "운동하다 바지 찢어졌다.",
         vote : 15,
-        writer : "채이스내",
+        writer : "루돌프",
         writedDate : "2023-09-06"
     },{
-        title : "프로틴 유머",
-        vote : 20,
-        writer : "이건아",
+        title : "헬창만 이해할 수 있음",
+        vote : 31,
+        writer : "김준수",
         writedDate : "2023-09-06"
     },{
-        title : "프로틴 유머",
-        vote : 20,
-        writer : "이건아",
+        title : "님들 이거 아심?",
+        vote : 25,
+        writer : "이선왕",
         writedDate : "2023-09-06"
     },
 ]
@@ -60,12 +60,12 @@ listArr.forEach(list => {
     let table = `
     <li class="line">
         <div>
-            <span id="title">${list.title}</span>
-            <span id="vote">추천수 : ${list.vote}</span>
+            <span id="title" style="font-size:19px;"><b>${list.title}</b></span>
+            <span id="vote" style="font-size:14px;">추천수 : ${list.vote}</span>
         </div>
         <div>
-            <span id="writer">${list.writer}</span>
-            <span id="date">${list.writedDate}</span>
+            <span id="writer" style="font-size:14px;">${list.writer}</span>
+            <span id="date" style="font-size:14px;">${list.writedDate}</span>
         </div>
     </li>
 `;
